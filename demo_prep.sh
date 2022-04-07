7e82794ff9bd36ed22405676ede0d6ce2d507738
Luis Alonso Montero <lumontero@mobilize.net>
2022-04-07 23:43:13 +0000

    Update demo prep script

--- demo_prep.sh
+++ demo_prep.sh
@@ -1,2 +1,3 @@
-mv scala-library-2.12.15.jar /home/BlackDiamond/.resources
+mkdir /home/BlackDiamond/.resources/
+mv scala-library-2.12.15.jar /home/BlackDiamond/.resources/
 mv demo_prep.sh /home/BlackDiamond/.resources
