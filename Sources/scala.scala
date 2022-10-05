437485b0e05c2822eb215fd159febe8c7f1d9bf2
ChristianCruzx <ChristianCruzx>
2022-10-05 22:40:12 +0000

    Auto save generated by BlackDiamond studio.

---
+++ Sources/scala.scala
@@ -0,0 +1,11 @@
+package com.mobilize.spark
+object Credentials {
+    val WAREHOUSE = "snowpark_test_wh"
+    val USER = "spark1"
+    val PASSWORD = "Test123."
+    val DB = "snowpark_testdb"
+    val ROLE = "snowpark_testdb_role"
+    val ACCOUNT = "mobilize"
+    val SCHEMA = "COMSCORE"
+}
+
